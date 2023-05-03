1f3700a46ba3893680e11e4a8778fac9f7e0cc05
Julian Kalinowski <kalinowskijulian@gmail.com>
2023-05-03 18:54:25 +0200

    try fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,3 @@ pluginManagement {
     includeBuild("plugin")
 }
 
-include("samples:ktor-fatjar-sample")
-include("samples:ktor-docker-sample")
-//include("samples:ktor-native-image-sample") // KTOR-4596 Disable Native image related tasks
