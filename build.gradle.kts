243f006501c899c1c7689dd4f3ac26d50391c289
Julian Kalinowski <kalinowskijulian@gmail.com>
2023-05-03 17:25:47 +0200

    try fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     @Suppress("DSL_SCOPE_VIOLATION") // "libs" produces a false-positive warning, see https://youtrack.jetbrains.com/issue/KTIJ-19369
     alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
+    java
 }
 
 repositories.mavenCentral()
 
 subprojects {
+    apply(plugin =  "maven-publish")
+    apply(plugin ="java")
     repositories.mavenCentral()
     tasks.withType<Test> {
         useJUnitPlatform()
@@ -19,4 +23,16 @@ subprojects {
         implementation(rootProject.libs.ktor.server.cio)
         implementation(rootProject.libs.logback)
     }
+    publishing {
+        publications{
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name.toString()
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
+    }
 }
+
+
